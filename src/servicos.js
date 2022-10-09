5656bb7fc4a0fbf968845768c453f2caba03b5e0
renserrano <renan_jau@yahoo.com.br>
2022-10-09 19:52:16 -0300

    Subindo Imagens

--- src/servicos.js
+++ src/servicos.js
@@ -4,14 +4,39 @@ const lista_servicos = [
         "servicos":[
             {
                 "nome":"Espaço Vida e Beleza",
-                "imagem":"imgs/unha001.jpg",
+                "imagem":"imgs/mao1.jpeg",
                 "valor": "R$ 35,00"
             },
             {
                 "nome":"Salão Impactus",
-                "imagem":"imgs/unha002.jpg",
+                "imagem":"imgs/mao2.jpeg",
                 "valor": "R$ 42,00"
-            }                        
+            },
+            {
+                "nome":"Espaço Dela",
+                "imagem":"imgs/mao3.jpeg",
+                "valor": "R$ 35,00"
+            },
+            {
+                "nome":"Bellas Estética",
+                "imagem":"imgs/mao4.jpeg",
+                "valor": "R$ 37,00"
+            },
+            {
+                "nome":"Dilma Manicure",
+                "imagem":"imgs/mao5.jpeg",
+                "valor": "R$ 35,00"
+            },
+            {
+                "nome":"Rosangela Estética",
+                "imagem":"imgs/mao6.jpeg",
+                "valor": "R$ 28,00"
+            },
+            {
+                "nome":"Espaço Carmen Lucia de Estética",
+                "imagem":"imgs/mao7.jpeg",
+                "valor": "R$ 29,00"
+            }                                    
         ]
     },
     {
