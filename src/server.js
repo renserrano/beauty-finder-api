01a506d51d6aae77ef2387e5b5f5a1fd1eb6c9d6
renserrano <renan_jau@yahoo.com.br>
2022-10-09 14:36:13 -0300

    Adicionando dados de servicos

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+const PORT = process.env.PORT || 5000; // const PORT = process.env.PORT || 8000 // this is for deploying heroku
 const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-    response.send('<span style="color: #ff00;">Hello World!</span>');
+    response.send('<span style="color: #ff00;">Api Beauty Finder</span>');
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
 });
+
+app.get('/servicos', (req, res) => {
+    res.json(getServicos());
+});
+
+async function getServicos(){
+    const servicos = await fetch("servicos.json");
+    return servicos;
+}
