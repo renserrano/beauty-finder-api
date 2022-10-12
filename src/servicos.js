0cae6a0e6f04dc95a8fa59bd2c8bcac43adc6679
renserrano <renan_jau@yahoo.com.br>
2022-10-12 09:59:17 -0300

    Update servicos.js

--- src/servicos.js
+++ src/servicos.js
@@ -5,37 +5,44 @@ const lista_servicos = [
             {
                 "nome":"Espaço Vida e Beleza",
                 "imagem":"imgs/mao1.jpeg",
-                "valor": "R$ 35,00"
+                "valor": "R$ 35,00",
+                "Endereco": "Rua São Januario, 896, Vila Seixas"
             },
             {
                 "nome":"Salão Impactus",
                 "imagem":"imgs/mao2.jpeg",
-                "valor": "R$ 42,00"
+                "valor": "R$ 42,00",
+                "Endereco": "Av Costa e Silva, 6.795, Maria Casagrande"
             },
             {
                 "nome":"Espaço Dela",
                 "imagem":"imgs/mao3.jpeg",
-                "valor": "R$ 35,00"
+                "valor": "R$ 35,00",
+                "endereco": "Rua Marcondes Salgado Filho, 563, Perimetral"
             },
             {
                 "nome":"Bellas Estética",
                 "imagem":"imgs/mao4.jpeg",
-                "valor": "R$ 37,00"
+                "valor": "R$ 37,00",
+                "endereco": "Rua Antonio Frederico, 396, Vila Albertina"
             },
             {
                 "nome":"Dilma Manicure",
                 "imagem":"imgs/mao5.jpeg",
-                "valor": "R$ 35,00"
+                "valor": "R$ 35,00",
+                "endereco": "Av Paulo Batalhão, 783, Js Paulista"
             },
             {
                 "nome":"Rosangela Estética",
                 "imagem":"imgs/mao6.jpeg",
-                "valor": "R$ 28,00"
+                "valor": "R$ 28,00",
+                "endereco": "Rua Plinio de Souza Castro, 926, Centro"
             },
             {
                 "nome":"Espaço Carmen Lucia de Estética",
                 "imagem":"imgs/mao7.jpeg",
-                "valor": "R$ 29,00"
+                "valor": "R$ 29,00",
+                "endereco": "Av José Paulo das Torres, 1.543, Jd Paulista"
             }                                    
         ]
     },
@@ -45,12 +52,14 @@ const lista_servicos = [
             {
                 "nome":"Amanda Esteticista",
                 "imagem":"imgs/salao1.jpeg",
-                "valor": "R$ 27,00"
+                "valor": "R$ 27,00",
+                "endereco": "Rua Patriarca, 896, Centro"
             },
             {
                 "nome":"Camila Mendes Estética e Cuidados da Mulher",
                 "imagem":"imgs/salao2.jpeg",
-                "valor": "R$ 50,00"
+                "valor": "R$ 50,00",
+                "endereco": "Av Antartica, 346, Centro"
             }                        
         ]
     },
@@ -60,12 +69,14 @@ const lista_servicos = [
             {
                 "nome":"Salão da Jualiana",
                 "imagem":"imgs/salao7.png",
-                "valor": "R$ 150,00"
+                "valor": "R$ 150,00",
+                "endereco": "Av Mauro Cavalini, 9203, Estação"
             },
             {
                 "nome":"Marta Cabeleireira",
                 "imagem":"imgs/salao6.png",
-                "valor": "R$ 120,00"
+                "valor": "R$ 120,00",
+                "endereco": "Av Coronel Barbosa Inacio, 438, Ipiranga"
             }                        
         ]
     },
@@ -75,22 +86,26 @@ const lista_servicos = [
             {
                 "nome":"Salão da Cidinha",
                 "imagem":"imgs/cabelo1.png",
-                "valor": "R$ 150,00"
+                "valor": "R$ 150,00",
+                "endereco": "Rua Mosaicos, 187, Estação"
             },
             {
                 "nome":"Espaço de Beleza Ezra Fernandes",
                 "imagem":"imgs/corte1.jpg",
-                "valor": "R$ 120,00"
+                "valor": "R$ 120,00",
+                "endereco": "Av Padre Augusto Cesar, 7847, Centro"
             },
             {
                 "nome":"Centro de Estética Marilda Genézia",
-                "imagem":"imgs/corte2.png",
-                "valor": "R$ 180,00"
+                "imagem":"imgs/corte3.jpg",
+                "valor": "R$ 180,00",
+                "endereco": "Rua Major Prado, 567, Centro"
             },
             {
                 "nome":"Salão da Naty",
                 "imagem":"imgs/corte3.jpg",
-                "valor": "R$ 220,00"
+                "valor": "R$ 220,00",
+                "endereco": "Avenia Molina, 1569, Vila Albertina"
             }                        
         ]
     },
